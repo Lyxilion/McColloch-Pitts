778a56891a751c49178c7de12b5108cda9ff97a3
Boucher Rubens <boucher.rubens.974@gmail.com>
2017-03-27 09:38:00 +0200

    Creation
    A neural Network using the McColloch-Pitts model.

---
+++ McColloch-Pitts.py
@@ -0,0 +1,84 @@
+def sgn(x):
+    if x<0:
+        return 0
+    else :
+        return 1
+
+def f(X,H,t):
+    """
+        fontion de tranfert de McColloch-Pitts
+    :param X: list : Connexxion ativatrice
+    :param H: list : Connecion inhibitrice
+    :param t: int :seuil
+    :return: Bool
+    """
+    x = 0
+    h = 0
+    for i in range(len(X)):
+        x += X[i]
+    for i in range(len(H)):
+        h += H[i]
+    if h == 0 :
+        return sgn(x - t)
+    else :
+        return 0
+
+
+"""
+Modelisation de la fonction répondant a cette table de vérité :
+ _________________
+| Entré | Sorties |
+|_________________|
+|  001  |   1     |
+|  011  |   1     |
+|  101  |   1     |
+|  111  |   1     |
+| Sinon |   0     |
+|_________________|
+"""
+
+X=[0,0,1]
+print(f(
+    [
+        f([X[2]],[X[0],X[1]],1),
+        f([X[1],X[2]],[X[0]],2),
+        f([X[0],X[2]],[X[1]],2),
+        f([X[0],X[1],X[2]],[0],3)
+    ]
+, [0], 1))
+X=[0,1,1]
+print(f(
+    [
+        f([X[2]],[X[0],X[1]],1),
+        f([X[1],X[2]],[X[0]],2),
+        f([X[0],X[2]],[X[1]],2),
+        f([X[0],X[1],X[2]],[0],3)
+    ]
+, [0], 1))
+X=[1,0,1]
+print(f(
+    [
+        f([X[2]],[X[0],X[1]],1),
+        f([X[1],X[2]],[X[0]],2),
+        f([X[0],X[2]],[X[1]],2),
+        f([X[0],X[1],X[2]],[0],3)
+    ]
+, [0], 1))
+X=[1,1,1]
+print(f(
+    [
+        f([X[2]],[X[0],X[1]],1),
+        f([X[1],X[2]],[X[0]],2),
+        f([X[0],X[2]],[X[1]],2),
+        f([X[0],X[1],X[2]],[0],3)
+    ]
+, [0], 1))
+X=[1,1,0]
+print(f(
+    [
+        f([X[2]],[X[0],X[1]],1),
+        f([X[1],X[2]],[X[0]],2),
+        f([X[0],X[2]],[X[1]],2),
+        f([X[0],X[1],X[2]],[0],3)
+    ]
+, [0], 1))
